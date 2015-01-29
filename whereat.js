627025e663ba082a92285741fc488f954e0dc55f
Matthew Skomarovsky <matthew@verypolite.com>
2015-01-28 19:41:50 -0500

    added geonames intersection lookup

--- whereat.js
+++ whereat.js
@@ -5,13 +5,52 @@ Router.configure({
 Router.route('/', function() {
   this.render('home', {
     data: function() {
-      return { cellyAppId: Meteor.settings.public.cellyAppId, userId: Meteor.userId() };
+      var coords = Geolocation.latLng();
+      console.log(coords);
+      return { 
+        cellyAppId: Meteor.settings.public.cellyAppId,
+        userId: Meteor.userId(),
+        coords: coords
+      };
     }
   });
 });
 
+Router.route('/shareLocation', function (){
+  var t = this;
+  var cellName = this.params.query.cellName;
+  var cellyToken = this.params.query.cellyToken;
+  var lng = t.params.query.lng;
+  var lat = t.params.query.lat;
+  var message = t.params.query.message;
+  var geode = Meteor.npmRequire('geode');
+  geo = new geode(Meteor.settings.geonamesUsername, { language: 'en', country : 'US' });
+  geo.request("findNearestIntersection", { lat: lat, lng: lng }, Meteor.bindEnvironment(function(err, results) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(results);
+      var boroMap = {
+        'Kings': 'Brooklyn',
+        'Queens': 'Queens',
+        'New York': 'Manhattan',
+        'Richmond': 'Statend Island',
+        'Bronx': 'The Bronx'
+      };
+      var data = results.intersection;
+      var boro = (data.adminName2 ? " in " + boroMap[data.adminName2] : "");
+      var text = (message ? message + " " : "") + "(I'm near " + data.street1 + " and " + data.street2 + boro + ")";
+      Meteor.call("shareLocation", cellyToken, cellName, text);
+    }
+    t.response.writeHead(302, { 'Location': "/" });
+    t.response.end();
+  }));
+}, { where: 'server' });
+
 Router.route('/cellyAuthCallback/:userId', function() {
   t = this;
+  var querystring = Npm.require('querystring');
+  var https = Npm.require('https');
   var userId = this.params.userId;
   var authCode = this.params.query.code;
   var postParams = {
@@ -20,9 +59,8 @@ Router.route('/cellyAuthCallback/:userId', function() {
     grant_type: 'authorization_code',
     code: authCode
   };
-  var querystring = Npm.require('querystring');
+  
   var postData = querystring.stringify(postParams);
-  var http = Npm.require('https');
   var postOptions = {
     hostname: 'cel.ly',
     port: 443,
@@ -32,7 +70,7 @@ Router.route('/cellyAuthCallback/:userId', function() {
       'User-Agent': 'Node.js'
     }    
   };
-  var req = http.request(postOptions, Meteor.bindEnvironment(function(result) { 
+  var req = https.request(postOptions, Meteor.bindEnvironment(function(result) { 
     result.setEncoding('utf8');    
     result.on('data', Meteor.bindEnvironment(function (chunk) {
       var data = JSON.parse(chunk);
@@ -46,7 +84,7 @@ Router.route('/cellyAuthCallback/:userId', function() {
   }));
   req.write(postData);
   req.end();
-}, {where: 'server'});
+}, { where: 'server' });
 
 if (Meteor.isClient) {
   Deps.autorun(function() {
@@ -54,7 +92,6 @@ if (Meteor.isClient) {
   });
 
   Template.home.events({
-    'click button': function() {}
   });
 }
 
@@ -66,6 +103,77 @@ if (Meteor.isServer) {
     if (!this.userId) return null;
     return Meteor.users.find(this.userId, {fields: {
       cellyToken: 1,
+      cellyCells: 1
     }});
   });  
 }
+
+Meteor.methods({
+  getCells: function() {
+    var https = Meteor.npmRequire('https');
+    var reqOptions = {
+      hostname: 'cel.ly',
+      port: 443,
+      path: '/api/me?oauth_token=' + Meteor.user().cellyToken,
+      method: 'GET',
+      headers: {
+        'User-Agent': 'Node.js'
+      }    
+    };
+    var req = https.request(reqOptions, Meteor.bindEnvironment(function(result) { 
+      result.setEncoding('utf8');    
+      result.on('data', Meteor.bindEnvironment(function (chunk) {
+        var data = JSON.parse(chunk);
+        console.log(data);
+      }));
+    }));
+    req.on('error', Meteor.bindEnvironment(function(e) {
+      console.log('celly token endpoint error:', e);
+    }));
+    req.end();
+    console.log(req);    
+  },
+
+  shareLocation: function (cellyToken, cellName, text) {
+    // if (!Meteor.userId()) {
+    //   throw new Meteor.Error("not-authorized");
+    // }
+
+    // if (!Meteor.user().cellyToken) {
+    //   throw new Meteor.error("no-celly-token");
+    // }
+    if (!text) {
+      text = "test message";
+    }
+
+    var https = Meteor.npmRequire('https');
+    var postParams = {
+      oauth_token: cellyToken,
+      message: { body: text }
+    };
+    var postData = JSON.stringify(postParams);
+    var postOptions = {
+      hostname: 'cel.ly',
+      port: 443,
+      path: '/api/cells/' + cellName + '/new-message',
+      method: 'POST',
+      headers: {
+        'User-Agent': 'Node.js',
+        'Content-Type': 'application/json'
+      }    
+    };
+    var req = https.request(postOptions, Meteor.bindEnvironment(function(result) { 
+      result.setEncoding('utf8');    
+      result.on('data', Meteor.bindEnvironment(function (chunk) {
+        var data = JSON.parse(chunk);
+        console.log(data);
+      }));
+    }));
+    req.on('error', Meteor.bindEnvironment(function(e) {
+      console.log('celly token endpoint error:', e);
+    }));
+    req.write(postData);
+    req.end();
+    console.log(req);
+  }
+});
